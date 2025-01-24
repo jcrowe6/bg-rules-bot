374cdc177a016e24927444b41e45f096e1283e33
Jeremiah Crowell <jeremiah.c2001@gmail.com>
2025-01-24 14:56:03 -0600

    basic RAG

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     app_name: str = "BGRB RAG Service"
-    env: str = "local"
+    env: str = "DEV"
     retrieval_service_url: str 
     generation_service_url: str
     model_config  = SettingsConfigDict(env_file=".env")
--- app/main.py
+++ app/main.py
@@ -5,33 +5,54 @@ from typing_extensions import Annotated
 
 import requests
 
-from . import config
+from .config import Settings
 
-class Query(BaseModel):
+class RAGQuery(BaseModel):
+    game: str
+    question: str
+
+class RAGResponse(BaseModel):
+    answer: str
+
+class RetrievalQuery(BaseModel):
     game: str
     question: str
 
 class RetrievalResponse(BaseModel):
     documents: list[str]
 
-class Response(BaseModel):
-    answer: str
+class GenerationQuery(BaseModel):
+    model: str = "llama3.1"
+    stream: bool = False
+    prompt: str
+
+class GenerationResponse(BaseModel):
+    response: str
 
 app = FastAPI()
-s = config.Settings()
 
 # @lru_cache
 def get_settings():
-    return config.Settings()
+    return Settings()
 
-@app.post("/query")
-async def query(query: Query, settings: Annotated[config.Settings, Depends(get_settings)]) -> Response:
+def get_prompt(question: str, documents: list[str]) -> str:
+    return  f"Rulebook lines:\n{'\n'.join(documents)}\n \
+            User question: \"{question}\"\n \
+            Instruction:\n \
+            Provide a short answer to the user's rules question, referencing the rulebook lines above. Don't preface it."
+
+@app.post("/query") 
+async def query(query: RAGQuery, settings: Annotated[Settings, Depends(get_settings)]) -> RAGResponse:
     # 1 Call retrieval service with game and question
-    print(settings)
-    url = "http://localhost:8001"
-    retr_response = RetrievalResponse.model_validate(requests.post(f"{url}/query", json={"game": query.game, "question": query.question}).json())
-
+    retr_query = RetrievalQuery(game=query.game, question=query.question)
+    retr_response_json = requests.post(f"{settings.retrieval_service_url}/query", json=retr_query.model_dump()).json()
+    retr_response = RetrievalResponse.model_validate(retr_response_json)
     # 2 Call LLM service with relevant snippets
-    
+    prompt = get_prompt(query.question, retr_response.documents)
+    print(prompt)
+    gen_query = GenerationQuery(prompt=prompt)
+    gen_response = requests.post(f"{settings.generation_service_url}/api/generate", json=gen_query.model_dump())
+    gen_response = GenerationResponse.model_validate(gen_response.json())
+
     # Return generated response
-    return Response(answer=f'{retr_response.documents[0]}')
+    return RAGResponse(answer=gen_response.response)
